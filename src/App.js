40615a2977bf5e15ee0e4a2bf6f356d9ee79319c
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-11-18 21:47:29 -0500

    import components to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import NavBar from './components/NavBar';
+import Header from './components/Header';
+import Footer from './components/Footer';
+// import FontAwesome from './components/FontAwesome'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>setup</p>
+      <NavBar />
+      <Header />
+      <Footer />
     </div>
   );
 }
