a2aca52c2f50b224051458d994dab21824311af1
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-11-18 21:37:54 -0500

    add rel='noreferrer' to footer <a> tags

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,11 @@ const Footer = () => {
   <footer className="wrapper">
     <p>v24.0.7</p>
     <div>
-      <a href="https://prepr.org/privacy-policy/" target="_blank">Privacy policy</a>
-      <a href="https://prepr.org/terms-of-use" target="_blank">Terms of use</a>
-      <a href="https://intercom.help/preprme/en/" target="_blank">Help Center</a>
+      <a href="https://prepr.org/privacy-policy/" target="_blank" rel="noreferrer">Privacy policy</a>
+      <a href="https://prepr.org/terms-of-use" target="_blank" rel="noreferrer">Terms of use</a>
+      <a href="https://intercom.help/preprme/en/" target="_blank" rel="noreferrer" >Help Center</a>
     </div>
-    <p>Powered by <a href="https://learnlab.ai/" target="_blank">LearnLab.ai</a></p>
+    <p>Powered by <a href="https://learnlab.ai/" target="_blank" rel="noreferrer" >LearnLab.ai</a></p>
 
   </footer>
   )
