6631d3c2ed4271d0560842c61091d9c9f2150e29
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-11-18 22:05:00 -0500

    add footer border

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,18 @@
 const Footer = () => {
   return (
-  <footer className="wrapper">
-    <p>v24.0.7</p>
-    <div>
-      <a href="https://prepr.org/privacy-policy/" target="_blank" rel="noreferrer">Privacy policy</a>
-      <a href="https://prepr.org/terms-of-use" target="_blank" rel="noreferrer">Terms of use</a>
-      <a href="https://intercom.help/preprme/en/" target="_blank" rel="noreferrer" >Help Center</a>
-    </div>
-    <p>Powered by <a href="https://learnlab.ai/" target="_blank" rel="noreferrer" >LearnLab.ai</a></p>
+    <footer className="arial">
+      <div className="footerContainer wrapper">
+        <p>v24.0.7</p>
+        <div>
+          <a href="https://prepr.org/privacy-policy/" target="_blank" rel="noreferrer">Privacy policy</a>
+          <a href="https://prepr.org/terms-of-use" target="_blank" rel="noreferrer">Terms of use</a>
+          <a href="https://intercom.help/preprme/en/" target="_blank" rel="noreferrer" >Help Center</a>
+        </div>
+        <p>Powered by <a href="https://learnlab.ai/" target="_blank" rel="noreferrer" >LearnLab.ai</a></p>
 
-  </footer>
+      </div>
+
+    </footer>
   )
 }
 
