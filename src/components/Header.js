e27ea0ca7fe2a4913f33206e58d44d5a657eadf3
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-11-18 21:41:33 -0500

    initial header setup - h1, p, h2

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+  <header>
+    <div>
+      <h1 className="poppins">Explore Your Interests</h1>
+      <p className='arial'>We’ll recommend content based on your interests we’ve listed here. <br /> Feel free to add or remove topics to customize your experience !</p>
+      <h2 className="poppins">Click an interest to narrow your recommendations.</h2>
+    </div>
+  </header>
+  )
+}
+
+export default Header;
