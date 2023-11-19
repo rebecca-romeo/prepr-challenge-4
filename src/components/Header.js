3794166e7fe305a0609d5ad6709e3b3f50208bb3
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-11-18 23:58:55 -0500

    nav bar and header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 const Header = () => {
   return (
-  <header>
-    <div>
-      <h1 className="poppins">Explore Your Interests</h1>
-      <p className='arial'>We’ll recommend content based on your interests we’ve listed here. <br /> Feel free to add or remove topics to customize your experience !</p>
-      <h2 className="poppins">Click an interest to narrow your recommendations.</h2>
-    </div>
-  </header>
+    <header>
+      <div>
+        <h1 className="poppins">Explore Your Interests</h1>
+        <p className='arial'>We’ll recommend content based on your interests we’ve listed here. <br /> Feel free to add or remove topics to customize your experience !</p>
+
+        <form className="headerSearchBar">
+          <label htmlFor="headerBarSearch"></label>
+          <input type="text" placeholder="Search skills" name="headerBarSearch" className="arial" />
+        </form>
+
+        <h2 className="poppins">Click an interest to narrow your recommendations.</h2>
+
+      </div>
+    </header>
   )
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,16 @@
 import preprLogo from '../assets/preprLogo.png'
+import userImg from '../assets/userImg.png'
 
 const NavBar = () => {
   return (
     <div className="navContainer">
-      <nav className="arial wrapper">
+      <nav className="arial">
 
         <div>
           <a href="https://preprlabs.org/explore"><img src={preprLogo} alt="prepr labs logo" className="preprLogo" /></a>
         </div>
 
-        <ul>
+        <ul  className="navBarText">
           <li><a href="https://preprlabs.org/explore">Explore</a></li>
           <li><a href="https://preprlabs.org/user/home">Dashboard</a></li>
           <li><a href="https://preprlabs.org/list/challenge">My <br /> Challenges</a></li>
@@ -22,16 +23,14 @@ const NavBar = () => {
           <li><i class="bi bi-bell-fill"></i></li>
           <li><i class="bi bi-chat-left-fill"></i></li>
           <li><i class="bi bi-question-circle-fill"></i></li>
-        </ul>
 
         <form className="navSearchBar">
           <label htmlFor="navBarSearch"></label>
           <input type="text" placeholder="Search..." name="navBarSearch" className="arial" />
         </form>
 
-        <ul className="navBarIcons">
-          <li><i class="bi bi-person-circle"></i></li>
-          <li><i class="bi bi-grid-3x3-gap-fill"></i></li>
+          <li className="userIcons"><img src={userImg} alt="" /></li>
+          <li className="userIcons"><i class="bi bi-grid-3x3-gap-fill"></i></li>
         </ul>
 
       </nav>
