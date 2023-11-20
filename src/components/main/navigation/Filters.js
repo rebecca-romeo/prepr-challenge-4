c3a48cca4d76b34e7a0bf7318c03cadd67c86bdb
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-11-20 03:39:41 -0500

    create Filters navigation component

---
+++ src/components/main/navigation/Filters.js
@@ -0,0 +1,13 @@
+const Filters = () => {
+  return (
+    <aside className="filters">
+      <h4 className='poppins bold'>Filters</h4>
+      <div>Duration</div>
+      <div>Level</div>
+      <div>Organizations</div>
+      <div>Skills</div>
+    </aside>
+  )
+}
+
+export default Filters;
