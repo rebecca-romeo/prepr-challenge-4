b2ce51dd0883170bb9276f624faeca89f3f92c89
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-11-20 03:40:01 -0500

    create Main Nav component

---
+++ src/components/main/navigation/MainNav.js
@@ -0,0 +1,14 @@
+const MainNav = () => {
+  return (
+    <nav className="navBarMainSection wrapper">
+      <ul className="poppins bold">
+        <li className='labs'>Labs</li>
+        <li className='challenges'>Challenges</li>
+        <li className='resources'>Resources</li>
+        <li className='projects'>Projects</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default MainNav;
