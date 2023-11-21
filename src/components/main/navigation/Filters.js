95b11b37091b259dbead98a0530ce8f9335dac1c
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-11-20 20:11:13 -0500

    add arrows to filter component

--- src/components/main/navigation/Filters.js
+++ src/components/main/navigation/Filters.js
@@ -1,11 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const Filters = () => {
   return (
-    <aside className="filters">
+    <aside className="filters bold">
       <h4 className='poppins bold'>Filters</h4>
-      <div>Duration</div>
-      <div>Level</div>
-      <div>Organizations</div>
-      <div>Skills</div>
+      <div> <FontAwesomeIcon icon="fa-solid fa-chevron-right" /> <span className='p-2'>Duration</span> </div>
+      <div> <FontAwesomeIcon icon="fa-solid fa-chevron-right" /> <span className='p-2'>Level</span></div>
+      <div> <FontAwesomeIcon icon="fa-solid fa-chevron-right" /> <span className='p-2'>Organizations</span></div>
+      <div> <FontAwesomeIcon icon="fa-solid fa-chevron-right" /> <span className='p-2'>Skills</span></div>
     </aside>
   )
 }
