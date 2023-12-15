de25c6ae62b1a2fc85f0fc31380d23f435cc5adb
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-12-15 05:47:07 -0500

    add ThemeContext and useState to create a dark mode feature

--- src/App.js
+++ src/App.js
@@ -4,16 +4,29 @@ import Main from './components/Main';
 import Footer from './components/Footer';
 import FontAwesome from './components/FontAwesome'
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { createContext, useState } from 'react';
 import './App.css';
 
+export const ThemeContext = createContext(null);
+
+
 function App() {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme((current) => (current === "light" ? "dark" : "light"));
+  };
+
   return (
-    <div className="App">
-      <NavBar />
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id={theme}>
+        <NavBar />
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    </ThemeContext.Provider>
+
   );
 }
 
