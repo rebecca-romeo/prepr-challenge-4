ddfbaf86aebafd2fbbb66bd740f725fda3458b21
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-12-15 15:27:53 -0500

    add accessbility button to app

---
+++ src/components/accessbility/AccessbilityButton.js
@@ -0,0 +1,12 @@
+const AccessibilityButton = () => {
+  return (
+    <div className="accessibilityContainer">
+      <span className="accessibilityButton">
+        <i class="bi bi-person-wheelchair"></i>
+      </span>
+    </div>
+
+  )
+}
+
+export default AccessibilityButton;
